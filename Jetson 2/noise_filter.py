1113a7c9d6680fa2b1d6dbfec1045de5cddb9458
rmcmurrer81 <robertmcmurrer@gmail.com>
2025-08-04 19:43:48 -0400

    Update noise_filter.py

--- Jetson 2/noise_filter.py
+++ Jetson 2/noise_filter.py
@@ -1,4 +1,3 @@
-# Very simple filter for demo purposes
 def is_noise(text):
-    noise_keywords = ["tv", "commercial", "episode", "netflix", "background", "news"]
-    return any(word in text.lower() for word in noise_keywords)
+    return len(text.split()) < 2 or "uh" in text.lower()
+
